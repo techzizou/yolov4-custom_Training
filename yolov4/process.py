b45c65c0522aeab16f309458ae8e9eedbb073b29
techzizou <76662366+techzizou@users.noreply.github.com>
2021-01-18 10:36:21 +0530

    Update process.py

--- yolov4/process.py
+++ yolov4/process.py
@@ -5,7 +5,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 print(current_dir)
 
-current_dir = '/content/darknet/data/obj'
+current_dir = 'data/obj'
 
 # Percentage of images to be used for the test set
 percentage_test = 10;
